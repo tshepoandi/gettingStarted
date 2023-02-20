9e2108472c3521734bc3f443d8cbc5af4ee46c73
tshepo <tmashiloane@2u.com>
2023-02-20 21:06:52 +0200

    added a form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,48 @@
+# class ArticlesController < ApplicationController
+#   def index
+#     @articles = Article.all
+#   end
+
+#   def show
+#     @article = Article.find(params[:id])
+#   end
+
+#   def new
+#     @article = Article.new
+#   end
+
+#   def create
+#     @article = Article.new(title:"...",body:"...")
+
+#     if @article.save
+#       redirect_to @article
+#     else
+#       render :new , status :unprocessable_entity
+#     end
+#   end
+# end
+
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
