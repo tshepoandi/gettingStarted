d8305dbfa6e368952906d455f8e663b877fe679f
tshepo <tmashiloane@2u.com>
2023-02-20 20:54:16 +0200

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-  #this was automatically generated because the controller command had no --skip-routes flag
-  get '/users' , to:"users#index"
-  get '/users/:id' , to: "users#showAuser"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # first page we get to see
   root "articles#index"
 
-  # page we path to
-  get "/articles" , to: "articles#index"
+  resources :articles
+  # get "/articles" , to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 
-  # this takes parameter of id , right after getting articles ...show would probably be a file we create.
-  get "/articles/:id", to: "articles#show"
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  # ===========================
+
+  # this was automatically generated because the controller command had no --skip-routes flag
+  get '/users' , to:"users#index"
+  get '/users/:id' , to: "users#showAuser"
+
+  # ===========================
 end
 
 
