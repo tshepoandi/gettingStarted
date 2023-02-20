81fb666a98c404045f9b7e1333f99c5de1da27e4
tshepo <tmashiloane@2u.com>
2023-02-20 12:15:33 +0200

    added path to articles/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # first page we get to see
   root "articles#index"
-  get "/articles" , to: "articles#index"
+
+  # page we path to
+  # get "/articles" , to: "articles#index"
+
+  # this takes parameter of id , right after getting articles ...show would probably be a file we create.
+  get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
