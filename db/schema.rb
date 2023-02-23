90bbdcfe8cbe03205473e965d5d0a7a30347021b
tshepo <tmashiloane@2u.com>
2023-02-23 10:29:16 +0200

    modified show file to have an add new comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_102619) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_064731) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_102619) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
