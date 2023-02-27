5eead432c8179a355b31074976a508f8fe4f687b
tshepo <tmashiloane@2u.com>
2023-02-27 09:17:18 +0200

    figured password is pre-configd

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
 
-  # http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
+    # @commenter = @articles.comments.create(comment_params) #trying to generate commenter name
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
