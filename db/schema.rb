a6df4ee5d607276eca82df96700fbf3262b81e85
tshepo <tmashiloane@2u.com>
2023-02-27 19:19:35 +0200

    not sure why its working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_100711) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_165122) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -37,5 +37,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_100711) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "articles"
 end
